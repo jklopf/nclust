7947c61500bb540b051fbc1177d83b7070e6be2e
Pratyaksha Wirapati <pratyaksha.wirapati@sib.swiss>
2021-03-03 14:48:05 +0100

    function to recover items of a branch

--- R/sec.R
+++ R/sec.R
@@ -1,3 +1,6 @@
+# convert to leaf node identifiers
+# `pos` can be a character vector of labels or integer vector of dendrogram
+#  coordinate
 pos2id <- function(clust,pos)
 {
   if(is.character(pos))
@@ -7,6 +10,10 @@ pos2id <- function(clust,pos)
 }
 
 
+# smallest enclosing cluster
+# nodes are leaf node identifiers
+# returning the branch identifier (0-based id)
+#
 sec <- function(
   clust,  # nclust object
   nodes   # node identifiers (2..N+1 for leafs, N+2..2N for branches)
@@ -25,4 +32,27 @@ sec <- function(
       commpath <- intersect(commpath,path)
     }
   commpath[1]
+}
+
+# a set of (labeled) leaf indices under a smallest-encolsing cluster
+# 
+secitems <- function(
+  clust,
+  seeds,labels=TRUE
+  )
+{
+  p <- pos2id( clust, seeds )
+  if(length(p)==0) return(NULL)
+  s <- 1+sec( clust, p )
+  oa <- clust$leftmost[s]+1
+  ob <- oa + clust$nleaf[s]-1;
+  if(labels)
+    {
+    if( !is.null(clust$labels) )
+      clust$labels[ clust$order[oa:ob]]
+    else
+      stop("cluster has no labels")
+    }
+  else
+    clust$order[oa:ob]
 }
